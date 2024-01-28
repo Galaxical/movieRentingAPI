fcecbaac34af5e46c6afd029bc024ec95572590e
Movie Renting API <azisomtochukwu@gmail.com>
2024-01-28 22:21:20 +0100

    added customer

--- moviesAPI.js
+++ moviesAPI.js
@@ -1,10 +1,11 @@
-class Movie {
+class Movie { //first class "movie"
   constructor(title, genre, stock) {
     this.title = title;
     this.genre = genre;
     this.stock = stock;
   }
 
+  //rent a movie method
   rentMovie() {
     if (this.stock > 0) {
       this.stock--;
@@ -13,7 +14,41 @@ class Movie {
       console.log(`${this.title} is out of stock.`);
     }
   }
+
 }
 
+//class customer
+class Customer {
+  constructor(name) {
+    this.name = name;
+    this.rentedMovies = [];
+  }
+
+   rentMovie(movie) {
+    if (movie.stock > 0) {
+      this.rentedMovies.push(movie.title);
+      movie.rentMovie();
+      console.log(`${this.name} has rented ${movie.title}.`);
+    } else {
+      console.log(`${movie.title} is out of stock.`);
+    }
+  }
+
+   returnMovie(movie) {
+    const index = this.rentedMovies.indexOf(movie.title);
+    if (index !== -1) {
+      this.rentedMovies.splice(index, 1);
+      movie.returnMovie();
+      console.log(`${this.name} has returned ${movie.title}.`);
+    } else {
+      console.log(`${this.name} did not rent ${movie.title}.`);
+    }
+  }
+}
+
+
 const movie1 = new Movie("Inception", "Sci-Fi", 5);
-const movie2 = new Movie("The Shawshank Redemption", "Drama", 3);
+const movie2 = new Movie("Peaky Blinders", "Crime", 3);
+
+const customer1 = new Customer ("Alice");
+const customer2 = new Customer ("John");
